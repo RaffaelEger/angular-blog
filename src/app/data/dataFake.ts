3505e033e0fafe6f8529f17e4d7b4cc125228056
RaffaelEger <raffaeloliveira99@gmail.com>
2023-10-15 15:37:30 -0300

    criei o repositorio git

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Lançamentos do HBO Max em outubro',
+    description: 'Hbo max anuncia lançamentos do mês de outubro, confira...',
+    photoCover:
+      'assets/freira2.webp',
+  },
+  {
+    id: '2',
+    title: 'Michael Caine anuncia aposentadoria aos 90 anos',
+    description: 'Após 7 décadas de carreira o vencedor de dois Oscars anuncia aposentadoria.',
+    photoCover:
+      'assets/michaelcaine2.webp',
+
+  },
+  {
+    id: '3',
+    title: 'Invencivel anuncia trailer para segunda temporada',
+    description: 'Mark de volta à ativa e multiverso marcam o trailer da segunda temporada de Invencível',
+    photoCover:
+      'assets/invencivel2.webp',
+  },
+  {
+    id: '4',
+    title: 'Drake lança For all the dogs, seu oitavo álbum',
+    description: 'Trabalho lançado traz parcerias como Bad Bunny, J. Cole, SZA, Lil Yachty e sample de Frank Ocean.',
+    photoCover:
+      'assets/drake2.webp',
+  },
+ 
+];
